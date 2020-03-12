cd01ab388e96baf6fb206470ae058c326f9e5827
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-03-12 21:14:45 +0700

    DRY on controllers

--- orchestrator/controllers/index.js
+++ orchestrator/controllers/index.js
@@ -2,7 +2,196 @@ const axios = require('axios')
 const Redis = require('ioredis')
 const redis = new Redis()
 
+const getData = (res, cache, server) => {
+  redis.get(cache)
+    .then(result => {
+      if (result) {
+        const response = JSON.parse(result)
+        res
+          .status(200)
+          .json(response)
+      } else {
+        return axios({
+          method: 'get',
+          url: server
+        })
+      }
+    })
+    .then(response => {
+      if (response) {
+        const lists = response.data
+        redis.set(cache, JSON.stringify(lists))
+        res
+          .status(200)
+          .json(lists)
+      }
+    })
+    .catch(({response}) => {
+      res
+        .status(response.status)
+        .json(response.data)
+    })
+}
+
+const addData = (res, payload, server, cache) => {
+  axios({
+    method: 'post',
+    url: server,
+    data: payload
+  })
+    .then(({data}) => {
+      redis.del('services')
+      redis.del(cache)
+      res
+        .status(201)
+        .json(data)
+    })
+    .catch(({response}) => {
+      res
+        .status(response.status)
+        .json(response.data)
+    })
+}
+
+const deleteItem = (res, id, server, cache) => {
+  axios({
+    method: 'delete',
+    url: `${server}/${id}`
+  })
+    .then(({data}) => {
+      redis.del('services')
+      redis.del(cache)
+      res
+        .status(200)
+        .json(data)
+    })
+    .catch(({response}) => {
+      res
+        .status(response.status)
+        .json(response.data)
+    })
+}
+
+const getOneItem = (res, id, cache, server) => {
+  redis.get(cache)
+    .then(result => {
+      if (result) {
+        const items = JSON.parse(result)
+        const item = items.filter(oneItem => oneItem._id === id)
+        if (item.length > 0) {
+          res
+            .status(200)
+            .json(item[0])
+        } else {
+          res
+            .status(404)
+            .json({
+              errObj: {msg: "Item not found in the database"}
+            })
+        }
+      } else {
+        return axios({
+          method: 'get',
+          url: `${server}/${id}`
+        })
+      }
+    })
+    .then(response => {
+      if (response) {
+        const items = response.data
+        res
+          .status(200)
+          .json(items)
+      }
+    })
+    .catch(({response}) => {
+      res
+        .status(response.status)
+        .json(response.data)
+    })
+}
+
+const editItem = (res, id, payload, server, cache) => {
+  axios({
+    method: 'patch',
+    url: `${server}/${id}`,
+    data: payload
+  })
+    .then(({data}) => {
+      redis.del('services')
+      redis.del(cache)
+      res
+        .status(201)
+        .json(data)
+    })
+    .catch(({response}) => {
+      res
+        .status(response.status)
+        .json(response.data)
+    })
+}
+
 module.exports = {
+  getMovies(req, res) {
+    const cache = 'movieCache'
+    getData(res, cache, process.env.MOVIESERVER)
+  },
+
+  getSeries(req, res) {
+    const cache = 'seriesCache'
+    getData(res, cache, process.env.TVSERVER)
+  },
+
+  addMovies(req, res) {
+    const data = req.body
+    const cache = 'movieCache'
+    addData(res, data, process.env.MOVIESERVER, cache)
+  },
+
+  addSeries(req, res) {
+    const data = req.body
+    const cache = 'seriesCache'
+    addData(res, data, process.env.TVSERVER, cache)
+  },
+  
+  deleteMovies(req, res) {
+    const { id } = req.params
+    const cache = 'movieCache'
+    deleteItem(res, id, process.env.MOVIESERVER, cache)
+  },
+
+  deleteSeries(req, res) {
+    const { id } = req.params
+    const cache = 'seriesCache'
+    deleteItem(res, id, process.env.TVSERVER, cache)
+  },
+
+  getOneMovie(req, res) {
+    const { id } = req.params
+    const cache = 'movieCache'
+    getOneItem(res, id, cache, process.env.MOVIESERVER)
+  },
+
+  getOneSeries(req, res) {
+    const { id } = req.params
+    const cache = 'seriesCache'
+    getOneItem(res, id, cache, process.env.TVSERVER)
+  },
+
+  editMovie(req, res) {
+    const { id } = req.params
+    const data = req.body
+    const cache = 'movieCache'
+    editItem(res, id, data, process.env.MOVIESERVER, cache)
+  },
+
+  editSeries(req, res) {
+    const { id } = req.params
+    const data = req.body
+    const cache = 'seriesCache'
+    editItem(res, id, data, process.env.TVSERVER, cache)
+  },
+
   getAllData(req, res) {
     redis.get('services')
       .then(result => {
@@ -38,276 +227,6 @@ module.exports = {
               .status(200)
               .json(result)
         }
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  getMovies(req, res) {
-    redis.get('movieCache')
-      .then(result => {
-        if (result) {
-          const response = JSON.parse(result)
-          res
-            .status(200)
-            .json(response)
-        } else {
-          return axios({
-            method: 'get',
-            url: process.env.MOVIESERVER
-          })
-        }
-      })
-      .then(response => {
-        if (response) {
-          const movies = response.data
-          redis.set('movieCache', JSON.stringify(movies))
-          res
-            .status(200)
-            .json(movies)
-        }
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  getSeries(req, res) {
-    redis.get('seriesCache')
-      .then(result => {
-        if (result) {
-          const response = JSON.parse(result)
-          res
-            .status(200)
-            .json(response)
-        } else {
-          return axios({
-            method: 'get',
-            url: process.env.TVSERVER
-          })
-        }
-      })
-      .then(response => {
-        if (response) {
-          const series = response.data
-          redis.set('seriesCache', JSON.stringify(series))
-          res
-            .status(200)
-            .json(series)
-        }
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  addMovies(req, res) {
-    const data = req.body
-    axios({
-      method: 'post',
-      url: process.env.MOVIESERVER,
-      data
-    })
-      .then(({data}) => {
-        redis.del('services')
-        res
-          .status(201)
-          .json(data)
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  addSeries(req, res) {
-    const data = req.body
-    axios({
-      method: 'post',
-      url: process.env.TVSERVER,
-      data
-    })
-      .then(({data}) => {
-        redis.del('services')
-        res
-          .status(201)
-          .json(data)
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  deleteMovies(req, res) {
-    const data = req.body
-    const { id } = req.params
-    axios({
-      method: 'delete',
-      url: `${process.env.MOVIESERVER}/${id}` ,
-      data
-    })
-      .then(({data}) => {
-        redis.del('services')
-        redis.del('movieCache')
-        res
-          .status(200)
-          .json(data)
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  deleteSeries(req, res) {
-    const data = req.body
-    const { id } = req.params
-    axios({
-      method: 'delete',
-      url: `${process.env.TVSERVER}/${id}` ,
-      data
-    })
-      .then(({data}) => {
-        redis.del('services')
-        redis.del('seriesCache')
-        res
-          .status(200)
-          .json(data)
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  getOneMovie(req, res) {
-    const { id } = req.params
-    redis.get('movieCache')
-      .then(result => {
-        if (result) {
-          const movies = JSON.parse(result)
-          const movie = movies.filter(item => item._id === id)
-          if (movie.length > 0) {
-            res
-              .status(200)
-              .json(movie[0])
-          } else {
-            res
-              .status(404)
-              .json({
-                errObj: {msg: "Item not found in the database"}
-              })
-          }
-        } else {
-          return axios({
-            method: 'get',
-            url: `${process.env.MOVIESERVER}/${id}`
-          })
-        }
-      })
-      .then(response => {
-        if (response) {
-          const movies = response.data
-          res
-            .status(200)
-            .json(movies)
-        }
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  getOneSeries(req, res) {
-    const { id } = req.params
-    redis.get('seriesCache')
-      .then(result => {
-        if (result) {
-          const allSeries = JSON.parse(result)
-          const series = allSeries.filter(item => item._id === id)
-          if (series.length > 0) {
-            res
-              .status(200)
-              .json(series[0])
-          } else {
-            res
-              .status(404)
-              .json({
-                errObj: {msg: "Item not found in the database"}
-              })
-          }
-        } else {
-          return axios({
-            method: 'get',
-            url: `${process.env.TVSERVER}/${id}`
-          })
-        }
-      })
-      .then(response => {
-        if (response) {
-          const series = response.data
-          res
-            .status(200)
-            .json(series)
-        }
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  editMovie(req, res) {
-    const data = req.body
-    const { id } = req.params
-    axios({
-      method: 'patch',
-      url: `${process.env.MOVIESERVER}/${id}`,
-      data
-    })
-      .then(({data}) => {
-        redis.del('services')
-        redis.del('movieCache')
-        res
-          .status(201)
-          .json(data)
-      })
-      .catch(({response}) => {
-        res
-          .status(response.status)
-          .json(response.data)
-      })
-  },
-
-  editSeries(req, res) {
-    const data = req.body
-    const { id } = req.params
-    axios({
-      method: 'patch',
-      url: `${process.env.TVSERVER}/${id}`,
-      data
-    })
-      .then(({data}) => {
-        redis.del('services')
-        redis.del('seriesCache')
-        res
-          .status(201)
-          .json(data)
       })
       .catch(({response}) => {
         res
