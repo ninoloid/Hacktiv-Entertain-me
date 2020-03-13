0897840df4d810936c1a42f450a2f777dc44090f
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-03-13 18:33:08 +0700

    crud done

--- apollo/app.js
+++ apollo/app.js
@@ -21,6 +21,16 @@ const typeDefs = gql`
     tags: [String]
   }
 
+  type MovieDeleteResult {
+    found: Int
+    deleted: Int
+  }
+
+  type SeriesDeleteResult {
+    found: Int
+    deleted: Int
+  }
+
   type Query {
     getMovies: [Movie]
     getSeries: [Series]
@@ -54,10 +64,22 @@ const typeDefs = gql`
     tags: String
   }
 
+  input UpdateSeriesInput {
+    _id: ID
+    title: String!
+    overview: String!
+    poster_path: String!
+    popularity: Float!
+    tags: String
+  }
+
   type Mutation {
     createMovie(movie: CreateMovieInput): Movie
     createSeries(series: CreateSeriesInput): Series
     updateMovie(movie: UpdateMovieInput): Movie
+    updateSeries(series: UpdateSeriesInput): Series
+    deleteMovie(id: ID): MovieDeleteResult
+    deleteSeries(id: ID): SeriesDeleteResult
   }
 `
 
@@ -101,6 +123,27 @@ const resolvers = {
       const { _id } = args.movie
       const { data } = await axios.patch(`http://localhost:3001/movies/${_id}`, args.movie)
       return data.movie
+    },
+
+    updateSeries: async (_, args) => {
+      const { _id } = args.series
+      const { data } = await axios.patch(`http://localhost:3001/movies/${_id}`, args.series)
+      return data.series
+    },
+
+    deleteMovie: async (_, args) => {
+      const { id } = args
+      const { data } = await axios.delete(`http://localhost:3001/movies/${id}`)
+      console.log(data)
+      return data
+    },
+
+    deleteSeries: async (_, args) => {
+      console.log(args)
+      const { id } = args
+      const { data } = await axios.delete(`http://localhost:3002/tv/${id}`)
+      console.log(data)
+      return data
     },
   }
 }
