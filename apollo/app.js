647250dffd6373440817e487d435d2b21ca4390f
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-03-13 18:10:51 +0700

    create, read, update

--- apollo/app.js
+++ apollo/app.js
@@ -27,6 +27,38 @@ const typeDefs = gql`
     getMovie(id: String): Movie
     getOneSeries(id: String): Series
   }
+
+    
+  input CreateMovieInput {
+    title: String!
+    overview: String!
+    poster_path: String!
+    popularity: Float!
+    tags: String
+  }
+
+  input CreateSeriesInput {
+    title: String!
+    overview: String!
+    poster_path: String!
+    popularity: Float!
+    tags: String
+  }
+
+  input UpdateMovieInput {
+    _id: ID
+    title: String!
+    overview: String!
+    poster_path: String!
+    popularity: Float!
+    tags: String
+  }
+
+  type Mutation {
+    createMovie(movie: CreateMovieInput): Movie
+    createSeries(series: CreateSeriesInput): Series
+    updateMovie(movie: UpdateMovieInput): Movie
+  }
 `
 
 const fetchData = async (port, type) => {
@@ -51,10 +83,29 @@ const resolvers = {
     getOneSeries: (_, args) => {
       return fetchData(3002, `tv/${args.id}`)
     },
+  },
+
+  Mutation: {
+    createMovie: async (_, args) => {
+      const { data } = await axios.post(`http://localhost:3001/movies`, args.movie)
+      return data.movie
+    },
+
+    createSeries: async (_, args) => {
+      console.log(args)
+      const { data } = await axios.post(`http://localhost:3002/tv`, args.series)
+      return data.series
+    },
+
+    updateMovie: async (_, args) => {
+      const { _id } = args.movie
+      const { data } = await axios.patch(`http://localhost:3001/movies/${_id}`, args.movie)
+      return data.movie
+    },
   }
 }
 
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`); 
 })
